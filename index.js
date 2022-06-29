a5eabeee8f783587d615e44e2c2f7eb8b8a61a80
Nate <ggn440@gmail.com>
2022-06-29 15:42:45 -0300

    terminado

---
+++ index.js
@@ -0,0 +1,48 @@
+let cambiodom = document.getElementById("texto");
+cambiodom.innerHTML = "Apreta el boton para registrarte"
+
+let estiloButton = document.getElementById('button');
+estiloButton.style.border = 'solid';
+estiloButton.style.textTransform = 'uppercase';
+
+const EventButton = document.getElementById('button');
+EventButton.onclick = () => {
+    let crear = prompt('¿Quiere crear una cuenta?\nSi \nNo')
+while (crear == 'si') {
+    const titular = prompt('Ingrese Nombre del titular');
+    const usuario = prompt('Ingrese un nombre de usuario');
+    const contraseña = prompt('Ingrese uan contraseña');
+    const cuentaNueva = new cuenta(titular, usuario, contraseña);
+    NateNet.agregarCuentas(cuentaNueva)
+    alert('Cuenta creada con exito!')
+    break;
+}
+
+console.log(NateNet.cuentas)
+}
+
+class Plataforma {
+    constructor(nombre, creacion, creador) {
+        this.nombre = nombre;
+        this.creacion = creacion;
+        this.creador = creador;
+        this.cuentas = [];
+    }
+    agregarCuentas(cuenta){
+        this.cuentas.push(cuenta);
+    }
+}
+
+const NateNet = new Plataforma('NateNet', 2022, 'Nate');
+
+class cuenta {
+    constructor(titular, usuario, contraseña){
+        this.titular = titular;
+        this.usuario = usuario;
+        this.contraseña = contraseña;
+    }
+}
+
+
+
+
